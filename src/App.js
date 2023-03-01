05dd572e12d29e9933925c8877ab2d925c1e15d2
Milind452 <mohapatra.milind@gmail.com>
2023-03-01 21:55:01 +0530

    Create global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+const AppWrapper = styled.div`
+    text-align: center;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <AppWrapper>Hello</AppWrapper>
+        </>
+    );
 }
 
 export default App;
