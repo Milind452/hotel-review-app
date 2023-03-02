ec4a19165a6120d2803814a5c1bc14ca3ea7b3b9
Milind452 <mohapatra.milind@gmail.com>
2023-03-02 22:44:38 +0530

    Add test for open form btn on navbar

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -14,7 +14,11 @@ test("The NavBar component should render with a title", () => {
 
 test("The NavBar component should respond to button clicks", () => {
     const mockFunction = jest.fn();
-    render(<NavBar goBack={mockFunction} />);
+    render(<NavBar goBack={mockFunction} openForm={mockFunction} />);
+
     fireEvent.click(screen.getByText("< Go Back"));
     expect(mockFunction).toHaveBeenCalled();
+
+    fireEvent.click(screen.getByText("+ Add Review"));
+    expect(mockFunction).toHaveBeenCalledTimes(2);
 });
