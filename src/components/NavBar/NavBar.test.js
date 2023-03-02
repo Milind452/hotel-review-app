f6a0dc587b44650a6ba804e9a97d40dbd39a4aec
Milind452 <mohapatra.milind@gmail.com>
2023-03-02 22:36:50 +0530

    Update test for navbar title

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,12 +1,13 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import NavBar from "./NavBar";
 
-test("The NavBar should render", () => {
+test("The NavBar component should render", () => {
     const view = render(<NavBar />);
     expect(view).toMatchSnapshot();
 });
 
-test("The NavBar should render with a title", () => {
-    const view = render(<NavBar title="Test Application" />);
-    expect(view).toMatchSnapshot();
+test("The NavBar component should render with a title", () => {
+    const title = "Test Application";
+    render(<NavBar title={title} />);
+    expect(screen.getByRole("heading")).toHaveTextContent(title);
 });
