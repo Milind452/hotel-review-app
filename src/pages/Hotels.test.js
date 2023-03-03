8d7f4f6b3ad0ce57a37a0d79f03099f0ccc6336f
Milind452 <mohapatra.milind@gmail.com>
2023-03-03 21:56:43 +0530

    Test if hotels are rendered

--- src/pages/Hotels.test.js
+++ src/pages/Hotels.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Hotels from "./Hotels";
 import HotelsContext from "../context/HotelsContext";
+import { BrowserRouter } from "react-router-dom";
 
 test("The Hotels component should render", async () => {
     const wrapper = ({ children }) => (
@@ -17,4 +18,27 @@ test("The Hotels component should render", async () => {
     );
     render(<Hotels />, { wrapper });
     expect(await screen.findByText("Loading...")).toBeVisible();
+});
+
+test("The Hotels components should render a list of hotels", async () => {
+    const wrapper = ({ children }) => (
+        <BrowserRouter>
+            <HotelsContext.Provider
+                value={{
+                    loading: false,
+                    error: "",
+                    hotels: [
+                        { id: 1, title: "Test Hotel 1", thumbnail: "" },
+                        { id: 2, title: "Test Hotel 2", thumbnail: "" },
+                    ],
+                    fetchHotels: jest.fn(),
+                }}
+            >
+                {children}
+            </HotelsContext.Provider>
+        </BrowserRouter>
+    );
+    render(<Hotels />, { wrapper });
+    expect(screen.queryByText("Loading...")).toBeNull();
+    expect(screen.getAllByRole("link").length).toBe(2);
 });
