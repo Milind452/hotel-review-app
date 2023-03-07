bc6f002bab53c0d801b7ef7d4945c346e698e483
Milind452 <mohapatra.milind@gmail.com>
2023-03-07 21:09:29 +0530

    Test filling review form

--- cypress/e2e/hotels.cy.js
+++ cypress/e2e/hotels.cy.js
@@ -12,4 +12,21 @@ describe("Cypress", () => {
         cy.get("[data-cy=addReview]").click();
         cy.location("pathname").should("include", "new");
     });
+
+    it("fills in and submits the form", () => {
+        cy.visit("http://localhost:3000");
+        cy.get("a").first().click();
+
+        cy.get("[data-cy=addReview]").click();
+        cy.location("pathname").should("include", "new");
+
+        cy.get("form").within(() => {
+            cy.get("input[name=title]").type("Test review");
+            cy.get("input[name=description]").type(
+                "Is a test review by Cypress"
+            );
+            cy.get("input[name=rating]").type(4);
+            cy.get("button").click();
+        });
+    });
 });
